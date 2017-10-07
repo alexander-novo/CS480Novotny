e8bfc1b6a6ff69cebf88522d8ef0c4384569885c
Antilectual <matthewwjohnson@nevada.unr.edu>
2017-10-06 20:40:02 -0700

    Updates for menu: Layout changes, Scrolling added, logarithmic slider, automatic hiding of less used controls

--- PA6/include/Menu.h
+++ PA6/include/Menu.h
@@ -6,8 +6,8 @@
 #include "window.h"
 #include "object.h"
 
-#define MIN_TIME_SCALE -5.0f
-#define MAX_TIME_SCALE  5.0f
+#define MIN_TIME_SCALE -1000.0f
+#define MAX_TIME_SCALE  1000.0f
 #define MIN_MOVE_SCALE  0.0f
 #define MAX_MOVE_SCALE  5.0f
 
